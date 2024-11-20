f860fd83792e228190bb32a769203fc61a5243ee
Marcelo <marcdfranc@gmail.com>
2024-11-20 00:31:47 +0000

    icluindo templates ao logger

--- cacheservice/logger/logger.go
+++ cacheservice/logger/logger.go
@@ -1,41 +1,56 @@
+// logger.go
 package logger
 
-type ILogger interface {
-	Log(level LogLevel, message string, errors ...error)
-}
+import (
+	"io"
+	"os"
+	"time"
+)
 
 type Logger struct {
-	output IOutput
-	Name   string
+	output    io.Writer
+	formatter *LogFormatter
+	Name      string
 }
 
-func NewLogger(output IOutput, name string) *Logger {
+func NewLogger(output io.Writer, name string, formatter *LogFormatter) *Logger {
 	return &Logger{
-		output: output,
-		Name:   name,
+		output:    output,
+		formatter: formatter,
+		Name:      name,
 	}
 }
 
-func (l *Logger) Log(level LogLevel, message string, errors ...error) {
-	l.output.Log(level, l.Name, message, errors...)
+func (l *Logger) Log(level LogLevel, data map[string]interface{}) {
+	// Adiciona campos padrão ao mapa de dados
+	data["Name"] = l.Name
+	data["Level"] = level.String()
+	data["Time"] = time.Now().Format(time.RFC3339)
+
+	logMessage := l.formatter.Format(data)
+	l.output.Write([]byte(logMessage))
+
+	if level == FATAL {
+		os.Exit(1)
+	}
 }
 
-func (l *Logger) LogInfo(message string) {
-	l.Log(INFO, message)
+func (l *Logger) LogInfo(data map[string]interface{}) {
+	l.Log(INFO, data)
 }
 
-func (l *Logger) LogDebug(message string) {
-	l.Log(DEBUG, message)
+func (l *Logger) LogDebug(data map[string]interface{}) {
+	l.Log(DEBUG, data)
 }
 
-func (l *Logger) LogWarning(message string) {
-	l.Log(WARN, message)
+func (l *Logger) LogWarning(data map[string]interface{}) {
+	l.Log(WARN, data)
 }
 
-func (l *Logger) LogError(message string, errors ...error) {
-	l.Log(ERROR, message, errors...)
+func (l *Logger) LogError(data map[string]interface{}) {
+	l.Log(ERROR, data)
 }
 
-func (l *Logger) LogFatal(message string, errors ...error) {
-	l.Log(FATAL, message, errors...)
+func (l *Logger) LogFatal(data map[string]interface{}) {
+	l.Log(FATAL, data)
 }
--- cacheservice/main.go
+++ cacheservice/main.go
@@ -5,20 +5,63 @@ import (
 	"cacheservice/logger"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Injeção de dependência do ConsoleOutput
-	output := &logger.ConsoleOutput{}
-	logInstance := logger.NewLogger(output, "main")
+	// Template de log corrigido e dinâmico
+	templateStr := `[{{.Name}}] Level: {{.Level }} Time: {{.Time }} Msg: {{.Message }}
+    {{range $key, $value := .ExtraData}} {{$key}}: {{ $value}}
+    {{end}}
+    `
+
+	formatter := logger.NewLogFormatter(templateStr)
+	logInstance := logger.NewLogger(os.Stdout, "main", formatter)
 
 	// Uso do Logger na aplicação
-	logInstance.LogInfo("Iniciando servidor...")
+	logInstance.LogInfo(map[string]interface{}{
+		"Message": "Iniciando servidor...",
+		"ExtraData": map[string]interface{}{
+			"SessionID": "abc123",
+			"IP":        "192.168.1.1",
+		},
+	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logInstance.LogInfo("Recebida requisição para /")
+		logInstance.LogInfo(map[string]interface{}{
+			"Message": "Recebida requisição para /",
+			"ExtraData": map[string]interface{}{
+				"Request": r.URL.Path,
+				"Method":  r.Method,
+			},
+		})
 		w.Write([]byte("Hello, World!"))
 	})
 
+	// Exemplos de Log com diferentes níveis
+	logInstance.LogDebug(map[string]interface{}{
+		"Message": "Debugando aplicação",
+		"ExtraData": map[string]interface{}{
+			"DebugData": "Detalhes de depuração",
+		},
+	})
+	logInstance.LogWarning(map[string]interface{}{
+		"Message": "Aviso: algo pode estar errado",
+		"ExtraData": map[string]interface{}{
+			"WarningCode": 1001,
+		},
+	})
+	logInstance.LogError(map[string]interface{}{
+		"Message": "Erro encontrado",
+		"ExtraData": map[string]interface{}{
+			"Error": "could not process data",
+		},
+	})
+	logInstance.LogFatal(map[string]interface{}{
+		"Message": "Erro fatal: o sistema irá parar",
+		"ExtraData": map[string]interface{}{
+			"Error": "Critical failure",
+		},
+	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
